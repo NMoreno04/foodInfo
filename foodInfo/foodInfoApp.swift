d9170020a366ef7524c0a6707de0a91828c74aca
Natalia Moreno <natlcs20@icloud.com>
2020-12-03 20:09:15 -0500

    Initial Commit

---
+++ foodInfo/foodInfoApp.swift
@@ -0,0 +1,17 @@
+//
+//  foodInfoApp.swift
+//  foodInfo
+//
+//  Created by Natalia Moreno on 2020-12-03.
+//
+
+import SwiftUI
+
+@main
+struct foodInfoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
