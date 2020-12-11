7676c7fd0320fe030dcee0830ea8122acce67227
Natalia Moreno <natlcs20@icloud.com>
2020-12-11 13:42:25 -0500

    added list of consumed items

--- foodInfo/MealDetail.swift
+++ foodInfo/MealDetail.swift
@@ -21,24 +21,34 @@ struct MealDetail: View {
             Text("What did you have for \(meal.name.lowercased())?")
             
             Form {
-                Picker(selection: $selectedItem,
-                       label: Text("I ate..."),
-                       content: {
-                        ForEach(0 ..< meal.items.count) { index in
-                            Text(meal.items[index].name)
-                        }
-                })
+                
+                Section {
+                    
+                    Picker(selection: $selectedItem,
+                           label: Text("I ate..."),
+                           content: {
+                            ForEach(0 ..< meal.items.count) { index in
+                                Text(meal.items[index].name)
+                            }
+                    })
+                    
+                    
+                    Button(action: {
+                        consumedItems.append(Consumed(item: meal.items[selectedItem],
+                                                      mealType: meal.name))
+                        print(consumedItems)
+                    }, label: {
+                        Text("Add item")
+                    })
+
+                }
                 
                 
-                Button(action: {
-                    consumedItems.append(Consumed(item: meal.items[selectedItem],
-                                                  mealType: meal.name))
-                    print(consumedItems)
-                }, label: {
-                    Text("Add item")
-                })
-                
-                
+                Section {
+                    List(consumedItems) { consumed in
+                        Text(consumed.item.name)
+                    }
+                }
                 
                 
             }
